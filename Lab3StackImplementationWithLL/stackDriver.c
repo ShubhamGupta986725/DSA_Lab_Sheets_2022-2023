18915afde05308085e8fb9a5c4a29ea82511b640
ShubhamGupta986725 <95865181+ShubhamGupta986725@users.noreply.github.com>
2023-02-09 21:55:31 +0530

    Add files via upload

---
+++ Lab3StackImplementationWithLL/stackDriver.c
@@ -0,0 +1,17 @@
+#include "stack.h"
+#include <stdio.h>
+
+
+element itoe (int i);
+int main()
+{
+    
+}
+
+element itoe (int i)
+{
+    element e;
+    e.int_value = i;
+    e.float_value = 0;
+    return e;
+}
